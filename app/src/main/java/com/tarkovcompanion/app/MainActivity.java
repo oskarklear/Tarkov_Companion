91fd5dd3c5536e56868ca3619b79d02acc410907
William Delo <55678535+wdelo@users.noreply.github.com>
2022-10-05 21:43:27 -0400

    Added Activity lifecycle functions and log statements

--- app/src/main/java/com/tarkovcompanion/app/MainActivity.java
+++ app/src/main/java/com/tarkovcompanion/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.tarkovcompanion.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("State", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("State", "onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("State", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("State", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("State", "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("State", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("State", "onDestroy");
     }
 }
