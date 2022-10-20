ad5381fb1ae38caba8f21b07522d7a856ff6203e
William Delo <55678535+wdelo@users.noreply.github.com>
2022-10-20 13:29:19 -0400

    Added limit and offset to default query

--- app/src/main/java/com/tarkovcompanion/app/QueryBuilder.java
+++ app/src/main/java/com/tarkovcompanion/app/QueryBuilder.java
@@ -2,7 +2,7 @@ package com.tarkovcompanion.app;
 
 public class QueryBuilder {
     // "{ \"query\": \"{ items { name description types avg24hPrice basePrice width height changeLast48hPercent iconLink link sellFor { price source } } }\" }";
-    private final static String defaultQuery = "{ \"query\": \"{ items { name description } }\" }";
+    private final static String defaultQuery = "{ \"query\": \"{ items(limit: 5 offset: 0) { name description } }\" }";
 
     public static String getDefaultQuery() {
         return defaultQuery;
