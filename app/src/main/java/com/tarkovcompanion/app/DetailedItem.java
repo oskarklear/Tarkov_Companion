df3ebc7bc7663dde14ff13c0ce2c24695c8840b0
William Delo <55678535+wdelo@users.noreply.github.com>
2022-11-30 18:35:12 -0500

    Tweaks and cleanup

--- app/src/main/java/com/tarkovcompanion/app/DetailedItem.java
+++ app/src/main/java/com/tarkovcompanion/app/DetailedItem.java
@@ -30,44 +30,17 @@ import retrofit2.http.Url;
 
 public class DetailedItem extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "name";
-    private static final String ARG_PARAM2 = "description";
-
-    private Activity activity;
-    private Context context;
     private ItemViewModel itemViewModel;
-
     private Item item;
 
     public DetailedItem() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailedItem.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailedItem newInstance(String param1, String param2) {
-        DetailedItem fragment = new DetailedItem();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activity = requireActivity();
-        itemViewModel = new ViewModelProvider((ViewModelStoreOwner) activity)
+        itemViewModel = new ViewModelProvider((ViewModelStoreOwner) requireActivity())
                 .get(ItemViewModel.class);
     }
 
--- app/src/main/java/com/tarkovcompanion/app/FavoritesPage.java
+++ app/src/main/java/com/tarkovcompanion/app/FavoritesPage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
@@ -28,38 +29,22 @@ import java.util.List;
  */
 public class FavoritesPage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
     private ItemViewModel itemViewModel;
     private ItemAdapter adapter;
     private RecyclerView recyclerView;
-    private Activity activity;
+    private FragmentActivity activity;
     private Context context;
 
     public FavoritesPage() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoritesPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavoritesPage newInstance(String param1, String param2) {
-        FavoritesPage fragment = new FavoritesPage();
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = requireContext();
-        activity =  requireActivity();
+        activity = requireActivity();
         itemViewModel = new ViewModelProvider((ViewModelStoreOwner) activity)
                 .get(ItemViewModel.class);
     }
